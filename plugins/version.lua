8d5fbc5894e4be24cd89ece089ff7bf707982b62
githabbot <m.githabbot@gmail.com>
2016-05-08 18:45:46 +0430

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,20 +1,7 @@
---------------------------------------------------
---      ____  ____ _____                        --
---     |    \|  _ )_   _|___ ____   __  __      --
---     | |_  )  _ \ | |/ ·__|  _ \_|  \/  |     --
---     |____/|____/ |_|\____/\_____|_/\/\_|     --
---                                              --
---------------------------------------------------
---                                              --
---       Developers: @Josepdal & @MaSkAoS       --
---     Support: @Skneos,  @iicc1 & @serx666     --
---                                              --
---------------------------------------------------
-
 do
 
 function run(msg, matches)
-  return 'DBTeam Bot V1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot\n\nDevelopers: @MaSkAoS @Josepdal\nSupport: @skneos @iicc1 @Serx666\nChannels: @DBTeamEN @DBTeamES\n\nCheckout: https://github.com/Josepdal/DBTeam\nGNU GPL v2 license.'
+  return 'boomrange bot Supergroups\nAn advanced Administration bot based on nod32/boomrangebot\n\nDevelopers: @xXDangerXx @senator_blackk\nSupport: @MmdAli_Z\nChannels: @gham_kade_new @juwst_jowke\n\ngood luck ;-)\nGNU GPL v2 license.'
 end
 
 return {
